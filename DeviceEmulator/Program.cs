dc07686f520c7b2e7064bc905aaddf2d2e583f2d
Michael Fery <michael.fery@outlook.com>
2018-02-05 12:02:55 +0100

    init device emulator project

---
+++ DeviceEmulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeviceEmulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
